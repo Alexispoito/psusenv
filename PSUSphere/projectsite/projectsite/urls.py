87ee0d34b3ca878c28e21af2de090de5f9c73a5c
Alexispoito <202080107@psu.palawan.edu.ph>
2025-04-05 16:05:26 +0800

    Program Views

--- PSUSphere/projectsite/projectsite/urls.py
+++ PSUSphere/projectsite/projectsite/urls.py
@@ -4,12 +4,13 @@ from studentorg.views import HomePageView, OrganizationList, OrganizationCreateV
 from studentorg.views import OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView
 from studentorg.views import StudentList, StudentCreateView, StudentUpdateView, StudentDeleteView
 from studentorg.views import CollegeList, CollegeCreateView, CollegeUpdateView, CollegeDeleteView
+from studentorg.views import ProgramList, ProgramCreateView, ProgramUpdateView, ProgramDeleteView
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
-    
+
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
@@ -29,6 +30,11 @@ urlpatterns = [
     path('college_list/add/', CollegeCreateView.as_view(), name='college-add'),
     path('college_list/<pk>/', CollegeUpdateView.as_view(), name='college-update'),
     path('college_list/<pk>/delete/', CollegeDeleteView.as_view(), name='college-delete'),
+
+    path('program_list/', ProgramList.as_view(), name='program-list'),
+    path('program_list/add/', ProgramCreateView.as_view(), name='program-add'),
+    path('program_list/<pk>/', ProgramUpdateView.as_view(), name='program-update'),
+    path('program_list/<pk>/delete/', ProgramDeleteView.as_view(), name='program-delete'),
 
 
 ]

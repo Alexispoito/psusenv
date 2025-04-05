efdb172a63ac351b3db6539b31415aa9064e4aae
Alexispoito <202080107@psu.palawan.edu.ph>
2025-04-05 13:56:28 +0800

    Org Member views

--- PSUSphere/projectsite/studentorg/models.py
+++ PSUSphere/projectsite/studentorg/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
@@ -42,4 +43,8 @@ class Student(BaseModel):
 class OrgMember(BaseModel):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    date_joined = models.DateField()
+    date_joined = models.DateField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.lastname}, {self.student.firstname}"

309451e1cb88125d1bee1855d024bcdc7ae70e53
Alexispoito <202080107@psu.palawan.edu.ph>
2025-04-05 15:53:15 +0800

    College Views

--- PSUSphere/projectsite/projectsite/urls.py
+++ PSUSphere/projectsite/projectsite/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 from studentorg.views import OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView
 from studentorg.views import StudentList, StudentCreateView, StudentUpdateView, StudentDeleteView
+from studentorg.views import CollegeList, CollegeCreateView, CollegeUpdateView, CollegeDeleteView
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
+    
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
@@ -22,6 +24,11 @@ urlpatterns = [
     path('student_list/add/', StudentCreateView.as_view(), name='student-add'),
     path('student_list/<pk>/', StudentUpdateView.as_view(), name='student-update'),
     path('student_list/<pk>/delete/', StudentDeleteView.as_view(), name='student-delete'),
+
+    path('college_list/', CollegeList.as_view(), name='college-list'),
+    path('college_list/add/', CollegeCreateView.as_view(), name='college-add'),
+    path('college_list/<pk>/', CollegeUpdateView.as_view(), name='college-update'),
+    path('college_list/<pk>/delete/', CollegeDeleteView.as_view(), name='college-delete'),
 
 
 ]

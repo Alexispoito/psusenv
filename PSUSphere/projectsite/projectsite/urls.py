9f270b2e23744b7ed25156bf49119b286b2e9d17
Alexispoito <202080107@psu.palawan.edu.ph>
2025-04-07 21:41:00 +0800

    Logout Only

--- PSUSphere/projectsite/projectsite/urls.py
+++ PSUSphere/projectsite/projectsite/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 from studentorg.views import OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView
 from studentorg.views import StudentList, StudentCreateView, StudentUpdateView, StudentDeleteView
 from studentorg.views import CollegeList, CollegeCreateView, CollegeUpdateView, CollegeDeleteView
 from studentorg.views import ProgramList, ProgramCreateView, ProgramUpdateView, ProgramDeleteView
 from studentorg import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -36,5 +37,6 @@ urlpatterns = [
     path('program_list/<pk>/', ProgramUpdateView.as_view(), name='program-update'),
     path('program_list/<pk>/delete/', ProgramDeleteView.as_view(), name='program-delete'),
 
-
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
